77ad3057022dbfd33e02db973a46de061e3a971c
Dreamer-archalo <bmbrsuper@gmail.com>
2026-09-14 17:54:27 +0200

    Wire gameplay AI and navigation managers

---
+++ dist/gameplay-upgrades.js
@@ -0,0 +1,27 @@
+import {ModernGameplay} from './modern-gameplay.js';
+import {PedestrianManager} from './pedestrian-manager.js';
+import {MinimapUI} from './minimap-ui.js';
+import {ZoneManager} from './zone-manager.js';
+import {MissionSystem} from './mission-system.js';
+import {PoliceAI,installPoliceAIHooks} from './police-ai.js';
+
+installPoliceAIHooks();
+const managers=new WeakMap();
+function suite(game){
+ if(!managers.has(game))managers.set(game,{
+  pedestrians:new PedestrianManager(),
+  minimap:new MinimapUI(),
+  zones:new ZoneManager(),
+  missions:new MissionSystem(),
+  police:new PoliceAI()
+ });
+ return managers.get(game);
+}
+const base=ModernGameplay.prototype.update;
+if(!ModernGameplay.prototype.__aiUiGameplayUpgrade){
+ ModernGameplay.prototype.__aiUiGameplayUpgrade=true;
+ ModernGameplay.prototype.update=function(dt){
+  base.call(this,dt);const m=suite(this);
+  m.pedestrians.update(this,dt);m.minimap.update(this,dt);m.zones.update(this,dt);m.missions.update(this,dt);m.police.update(this,dt);
+ };
+}
