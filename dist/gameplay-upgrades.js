d1fe0b107fe43c460a0ca1ae3ee4bd372dbb479f
Dreamer-archalo <bmbrsuper@gmail.com>
2026-09-15 20:43:03 +0200

    Load second tangenziale race mode

--- dist/gameplay-upgrades.js
+++ dist/gameplay-upgrades.js
@@ -7,6 +7,7 @@ import {PoliceAI,installPoliceAIHooks} from './police-ai.js';
 import './tangenziale-race.js';
 import './tangenziale-race-polish.js';
 import './tangenziale-race-short-sprint.js';
+import './tangenziale-race-second.js';
 import './taxi-loading-guard.js';
 
 installPoliceAIHooks();
