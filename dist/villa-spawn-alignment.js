f244c839675283c699d91e1c89eeaa436fc96228
Dreamer-archalo <bmbrsuper@gmail.com>
2026-09-19 21:21:23 +0200

    Keep relocated Mandria vehicle respawns aligned to estate terrain

--- dist/villa-spawn-alignment.js
+++ dist/villa-spawn-alignment.js
@@ -1,4 +1,5 @@
 import {ModernGameplay} from './modern-gameplay.js';
+import {VILLA_PUBLIC_NAME} from './villa-mandria-relocation.js';
 
 const populate=ModernGameplay.prototype.populate;
 if(!ModernGameplay.prototype.__villaSpawnSurfaceAlignment){
@@ -6,7 +7,7 @@ if(!ModernGameplay.prototype.__villaSpawnSurfaceAlignment){
  ModernGameplay.prototype.populate=function(...args){
   const result=populate.apply(this,args);
   for(const c of this.cars){
-   if(!c.fixedSpawn||c.home?.name!=='Villa Treves')continue;
+   if(!c.fixedSpawn||c.home?.name!==VILLA_PUBLIC_NAME)continue;
    const platform=this.terrain.platformAt?.(c.x,c.z);const reference=platform?.height??c.y;
    const y=this.terrain.height(c.x,c.z,reference);
    c.y=y;c.speed=0;c.parked=true;c.home={...c.home,y};c.mesh.visible=true;
