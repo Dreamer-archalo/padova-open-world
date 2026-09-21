6e45259b6c967ee4c63aa383c2a7c616e73fd1a3
Dreamer-archalo <bmbrsuper@gmail.com>
2026-09-21 14:27:37 +0200

    Activate validated estate perimeter mounted and Ape Car circuits

--- dist/villa-mandria-v7-animal-motion.js
+++ dist/villa-mandria-v7-animal-motion.js
@@ -4,6 +4,7 @@ import './villa-mandria-v8-scope-targets.js';
 import './villa-mandria-v8-estate-tasks.js';
 import './villa-mandria-v8-grounds-roof.js';
 import './villa-mandria-v8-deliveries.js';
+import './villa-mandria-v8-perimeter-patrols.js';
 import {ModernGameplay} from './modern-gameplay.js';
 import {VILLA,areaPoint,areaLocal} from './gameplay-areas.js';
 import {mandriaFree} from './villa-mandria-placement-fix.js';
