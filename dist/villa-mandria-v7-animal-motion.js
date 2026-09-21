fc7e16851cd2c146d3e1f3a2382226f3bb6c823d
Dreamer-archalo <bmbrsuper@gmail.com>
2026-09-21 16:06:15 +0200

    Hook v9 autonomous estate activity after established Mandria animal update

--- dist/villa-mandria-v7-animal-motion.js
+++ dist/villa-mandria-v7-animal-motion.js
@@ -6,6 +6,7 @@ import './villa-mandria-v8-vtol.js';
 import './villa-mandria-v8-grounds-roof.js';
 import './villa-mandria-v8-deliveries.js';
 import './villa-mandria-v8-perimeter-patrols.js';
+import {mandriaV9Update} from './villa-mandria-v9-estate-life.js';
 import {ModernGameplay} from './modern-gameplay.js';
 import {VILLA,areaPoint,areaLocal} from './gameplay-areas.js';
 import {mandriaFree} from './villa-mandria-placement-fix.js';
@@ -36,4 +37,4 @@ function move(g,dt){if(!g.state?.started||!g.villaV7Life||!Number.isFinite(dt)||
  }
 }
 const oldUpdate=ModernGameplay.prototype.update;
-if(!ModernGameplay.prototype.__mandriaV7AnimalMotion){ModernGameplay.prototype.__mandriaV7AnimalMotion=true;ModernGameplay.prototype.update=function(dt){oldUpdate.call(this,dt);move(this,dt);};}
+if(!ModernGameplay.prototype.__mandriaV7AnimalMotion){ModernGameplay.prototype.__mandriaV7AnimalMotion=true;ModernGameplay.prototype.update=function(dt){oldUpdate.call(this,dt);move(this,dt);mandriaV9Update(this,dt);};}
